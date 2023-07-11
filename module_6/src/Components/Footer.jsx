0e5f98c25b1f2e45496448f082c23287a5bb8e2f
Md.Hasanat Zamil <zamil_surfserver@yahoo.com>
2023-07-11 21:54:00 +0600

    Update Footer

--- module_6/src/Components/Footer.jsx
+++ module_6/src/Components/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
     
 </div> 
   <div className='md:grid-cols-3  gap-12 grid-col-8 grid  '>
-     I am Hasanat Zamil. I am fullstacked web developer. I have 18years of experience with web programming. 
+     I am Hasanat Zamil. I am full stack web developer. I have 18years of experience with web programming. 
      Now I am update myself with React Js. I am a web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do.
   </div> 
    
