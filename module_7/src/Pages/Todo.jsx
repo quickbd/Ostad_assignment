1e9e4927eea6b27ba3d35e7325691649189d2eba
Md.Hasanat Zamil <zamil_surfserver@yahoo.com>
2023-07-18 00:20:17 +0600

    update todo

--- module_7/src/Pages/Todo.jsx
+++ module_7/src/Pages/Todo.jsx
@@ -104,7 +104,7 @@ const Todo = () => {
           <div className=" ">
             <div className="block rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
               <div className="border-b-2 text-xl border-neutral-100 px-6 py-3 dark:border-neutral-600 dark:text-neutral-50">
-                Completed Tasks
+                Completed Task
               </div>
               <div className="p-6 pt-0">
                 <ul className="max-w-md divide-y-reverse divide-y  divide-gray-200 dark:divide-gray-700">
